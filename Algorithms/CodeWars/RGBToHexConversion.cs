a35894b58b9f7c0b802d6113f9ae53835ad83708
Denis <dekkedenis@mail.ru>
2023-09-06 10:20:59 +0300

    WordMatch

---
+++ Algorithms/CodeWars/RGBToHexConversion.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace Algorithms.CodeWars;
+
+public static class RGBToHexConversion
+{
+    public static string Rgb(int r, int g, int b)
+    {
+        if (r > 255) r = 255;
+        if (g > 255) g = 255;
+        if (b > 255) b = 255;
+        if (r < 0) r = 0;
+        if (g < 0) g = 0;
+        if (b < 0) b = 0;
+        var myColor = Color.FromArgb(r, g, b);
+        var hex = myColor.R.ToString("X2") + myColor.G.ToString("X2") + myColor.B.ToString("X2");
+        return hex;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Rgb(255, 255, 300));
+    }
+}
--- Algorithms/WordMatch.cs
+++ Algorithms/WordMatch.cs
@@ -2,26 +2,51 @@
 
 public static class WordMatch
 {
-    private static void Main()
+    public static void Main()
     {
         const string wrongWord = "fosh";
-        var dictionary = new[] { "fish", "vista" }; 
-        FindMatch(wrongWord, dictionary);
+        var dictionary = new[] { "fish", "vista", "fort"};
+        Console.WriteLine(FindMatch(wrongWord, dictionary));
     }
 
     public static string FindMatch(string wrongWord, string[] dictionary)
     {
+        var fondedMax = new int[dictionary.Length];
         for (var i = 0; i < dictionary.Length; i++)
         {
-            var matrix = new int[wrongWord.Length,dictionary[i].Length];
-            for (var j = 0; j < wrongWord.Length; j++)
+            var matrix = new int[dictionary[i].Length, wrongWord.Length];
+            InitMatrix(matrix, wrongWord, dictionary[i]);
+            for (var j = 1; j < dictionary[i].Length; j++)
             {
-                for (var k = 0; k < dictionary[i].Length; k++)
+                for (var k = 1; k <  wrongWord.Length; k++)
                 {
-                    
+                    if (wrongWord[k] == dictionary[i][j])
+                    {
+                        var value = matrix[j - 1, k - 1] + 1;
+                        matrix[k, j] = value;
+                    }
+                    else matrix[j, k] = Math.Max(matrix[j, k - 1], matrix[j - 1, k]);
                 }
             }
+            fondedMax[i] = matrix[dictionary[i].Length - 1,wrongWord.Length - 1];
         }
-        return "";
+        var maxIndex = Array.IndexOf(fondedMax, fondedMax.Max());
+        return dictionary[maxIndex];
+    }
+
+    private static void InitMatrix(int[,] matrix, string wrongWord, string expectedWord)
+    {
+        if (wrongWord[0] == expectedWord[0]) matrix[0, 0] = 1;
+        for (var i = 1; i < wrongWord.Length; i++)
+        {
+            if (wrongWord[i] == expectedWord[0]) matrix[0, i] += 1;
+            else matrix[0, i] = matrix[0, i - 1];
+        }
+
+        for (var i = 1; i < expectedWord.Length; i++)
+        {
+            if (wrongWord[0] == expectedWord[i]) matrix[i, 0] += 1;
+            else matrix[i, 0] = matrix[i - 1, 0];
+        }
     }
 }
